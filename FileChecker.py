c6266a6a4e85f17e90076b77e8851bd6336af9fc
SardineBob <yushlai1987@gmail.com>
2020-09-14 16:37:19 +0800

    改成執行一次只checker一次

--- FileChecker.py
+++ FileChecker.py
@@ -4,13 +4,13 @@ from utilset.ConfigUtil import ConfigUtil
 from component.Checker import doFileChecker
 
 print("開始執行檔案雜湊比對檢查。")
-interval = ConfigUtil().ScanInterval
-active = True
-try:
-    while active:
-        doFileChecker()
-        time.sleep(interval)
-except KeyboardInterrupt:
-    active = False
+#interval = ConfigUtil().ScanInterval
+#active = True
+#try:
+#    while active:
+doFileChecker()
+#        time.sleep(interval)
+#except KeyboardInterrupt:
+#    active = False
 print("停止執行檔案雜湊檢查")
-input("按一下enter離開")
+#input("按一下enter離開")
